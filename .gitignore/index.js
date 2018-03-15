7e39eaf5927d95bbc2d60d102aefa9b83d87ea7d
Sixelafish <37385880+Sixelafish@users.noreply.github.com>
2018-03-15 22:17:41 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,10 +1,10 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 var prefix =  ("*")
 
-bot.on('ready', function()) {
-	bot.user.setFame("Command: *help");
+bot.on('ready', function() {
+	bot.user.setGame("Command: *help");
 	console.log("Connected");
 });
 
