88903f846379c31b7fb0652444c02ae08f7e4953
Sixelafish <37385880+Sixelafish@users.noreply.github.com>
2018-03-16 17:07:39 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -5,18 +5,17 @@ var prefix =  ("*")
 
 bot.on('ready', function() {
 	bot.user.setGame("Command: *help");
-	console.log("Connected");
+	console.log("Connectedç");
 });
 
-bot.login("process.env.TOKEN");
+bot.login(process.env.TOKEN");
 
 bot.on('message', message => {
 	if (message.content === prefix + "help"){
 		message.channel.sendMessage("Liste des commandes: /n *help - Affiche les commandes d'aides, idiot. /n *insulte - répertoire d'insultes moyenâgeuses.")
-	}
 
+}
 
-bot.on('message', message => {
 	if (message.content === prefix + "insulte")
 		message.channel.sendMessage("- Manant /n - Gredin /n - Sang-bleu")
 })
