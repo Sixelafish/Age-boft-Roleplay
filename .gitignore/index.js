1b4af7580f056d3fa5ec42157ec4f3fb04cf194c
Sixelafish <37385880+Sixelafish@users.noreply.github.com>
2018-03-16 17:26:57 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -12,10 +12,13 @@ bot.login(process.env.TOKEN);
 
 bot.on('message', message => {
 	if (message.content === prefix + "help"){
-		message.channel.sendMessage("```\n Liste des commandes: \n *help - Affiche les commandes d'aides, idiot. \n *insulte - répertoire d'insultes moyenâgeuses.```")
+		message.channel.sendMessage("```\n Liste des commandes: \n *help - Affiche les commandes d'aides, idiot. \n *insulte - Répertoire d'insultes moyenâgeuses. \n *flagellation - Permet de se néttoyer de tout ses pêchers.```")
 
 }
 
 	if (message.content === prefix + "insulte")
 		message.channel.sendMessage("```\n Listes d'insultes courantes : \n - Manant \n - Gredin \n - Sang-bleu```")
+
+	if (message.content === prefix + "flagellation")
+		message.channel.sendMessage("Vous avez pêcher, vous allez être punis ! \n https://cdn.discordapp.com/attachments/331837845851340800/424242014163304449/telechargement.jpg")
 })
