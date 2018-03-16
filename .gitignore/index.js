3a26155ba6d73534e6666818c8e93bc34c58b5fc
Sixelafish <37385880+Sixelafish@users.noreply.github.com>
2018-03-16 17:42:53 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -14,18 +14,18 @@ bot.on('message', message => {
 	if (message.content === prefix + "help"){
 		message.channel.sendMessage("```\n Liste des commandes: \n *help - Affiche les commandes d'aides, idiot. \n *insulte - Répertoire d'insultes moyenâgeuses. \n *flagellation - Permet de se néttoyer de tout ses pêchers.```")
 
-}
+	}
 
 	if (message.content === prefix + "insulte")
 		message.channel.sendMessage("```\n Listes d'insultes courantes : \n - Manant \n - Gredin \n - Sang-bleu```")
 
 	if (message.content === prefix + "flagellation")
-		aleatoire=Math.floor(Math.random()*5)
+		const aleatoire = Math.floor(Math.random()*5)
 			switch(aleatoire){
 			case 0:document.write("D");break;
 			case 1:document.write("A");break;
 			case 2:document.write("B");break;
 			case 3:document.write("C");break;
 			case 4:document.write("E");break;
-}
+			}
 })
