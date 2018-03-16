f8398baf6112fd2e24345c932cc6a9bbec03bd79
Sixelafish <37385880+Sixelafish@users.noreply.github.com>
2018-03-16 18:17:51 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -16,15 +16,5 @@ bot.on('message', message => {
 
 	if (message.content === prefix + "insulte")
 		message.channel.sendMessage("```\n Listes d'insultes courantes : \n - Manant \n - Gredin \n - Sang-bleu```")
-
-	if (message.content === prefix + "flagellation")
-		const aleatoire = Math.floor(Math.random()*5)
-			switch(aleatoire){
-			case 0:message.channel.sendMessage("D")
-			case 1:message.channel.sendMessage("A")
-			case 2:message.channel.sendMessage("B")
-			case 3:message.channel.sendMessage("C")
-			case 4:message.channel.sendMessage("E")
-			}
 			
 }
