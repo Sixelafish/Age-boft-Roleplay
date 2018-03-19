a69df98546e5e987609e94fb4da161c3f7529792
Sixelafish <37385880+Sixelafish@users.noreply.github.com>
2018-03-19 20:39:36 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,17 +4,19 @@ const bot = new Discord.Client();
 var prefix =  ("*")
 
 bot.on('ready', function() {
-	bot.user.setGame("Command: *help");
-	console.log("Connectedç");
+	bot.user.setGame("Commande: *help");
+	console.log("Connected");
 });
 
 bot.login(process.env.TOKEN);
 
 bot.on('message', message => {
-	if (message.content === prefix + "help");
-		message.channel.sendMessage("```\n Liste des commandes: \n *help - Affiche les commandes d'aides, idiot. \n *insulte - Répertoire d'insultes moyenâgeuses. \n *flagellation - Permet de se néttoyer de tout ses pêchers.```")
+	if (message.content === prefix + "help"){
+		message.channel.sendMessage("```\n Liste des commandes: \n *help - Affiche les commandes d'aides, idiot. \n *insulte - Répertoire d'insultes moyenâgeuses. \n *flagellation - Permet de se néttoyer de tout ses pêchers.```");
+	}
 
-	if (message.content === prefix + "insulte");
-		message.channel.sendMessage("```\n Listes d'insultes courantes : \n - Manant \n - Gredin \n - Sang-bleu```")
+	if (message.content === prefix + "insulte"){
+		message.channel.sendMessage("```\n Listes d'insultes courantes : \n - Manant \n - Gredin \n - Sang-bleu```");
+	}
 			
-}
+});
