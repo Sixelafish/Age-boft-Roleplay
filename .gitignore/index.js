af3c56090db1281693b1d0d296185e8460ae1a7f
Sixelafish <37385880+Sixelafish@users.noreply.github.com>
2018-03-25 13:16:42 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -21,7 +21,7 @@ bot.on('message', message => {
 
 	if (message.content === prefix + "flagellation"){
 		var coups = Math.floor(Math.random() * 451);
-		message.channel.send([message.author.id] + " tu à pêcher ! Prend ces " + coups + " coups de fouet")
+		message.channel.send("@" + [message.author] + " tu à pêcher ! Prend ces " + coups + " coups de fouet")
 		message.channel.send({ files: [{ attachment: "https://cdn.discordapp.com/attachments/331837845851340800/424242014163304449/telechargement.jpg", name: 'telechargement.jpg' }] })
 			}
 	}
